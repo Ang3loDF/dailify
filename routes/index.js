87da6e9fb7a106e0814e380b8c061347c5e2e512
Ang3loDF <angelodifuccia02@gmail.com>
2020-04-19 10:31:48 +0200

    create a news finders class and fix the problem of news duplication

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require("express"),
     router = express.Router(),
-    NYTFinder = require("../news-finders/NYTFinder"),
-    CNNFinder = require("../news-finders/CNNFinder"),
-    CNBCFincer = require("../news-finders/CNBCFinder");
+    NYTFinder = require("../lib/news-finders/NYTFinder"),
+    CNNFinder = require("../lib/news-finders/CNNFinder"),
+    CNBCFinder = require("../lib/news-finders/CNBCFinder"),
+    WSJFinder = require("../lib/news-finders/WSJFinder");
 
 router.get("/", function(req, res){
     res.send("<h1>Welcome to the home page</h1>");
@@ -12,7 +13,8 @@ router.get("/find", async function(req, res){
     var newNews = [];
     newNews = await NYTFinder.find(newNews);
     newNews = await CNNFinder.find(newNews);
-    newNews = await CNBCFincer.find(newNews);
+    newNews = await CNBCFinder.find(newNews);
+    newNews = await WSJFinder.find(newNews);
     console.log(newNews + " " + newNews.length);
     res.send("<p>Finding...</p>");
 })
